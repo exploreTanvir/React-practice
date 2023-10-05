afc38ad2a65ca932e934255b53885f6c57cc291e
Tanvir_Hossen <tanvirhossen853@gmail.com>
2023-10-05 22:28:55 +0600

    Functional component and use of jsx

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Conponents/Card";
+function App(){
+    return <div>
+        <h1 className="headingCss">Todo App</h1>
+        <Card/>
+    </div>
+}
+
+
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,49 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from "./app";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-// Use of jsx 
-// apply html js and css code using jsx
-
-const todoTitle="Style using jsx css"
-const todoDesc="Hello this is Description"
-const currentYear=new Date().getFullYear()
-const currentDate=new Date().getDate()
-const currentMonth=new Date().getMonth()
-const currentTime=new Date().getTime()
-
-// inline css
-const firstCss={color:"red",
-                fontSize:"3rem",
-                backgroundColor:"purple",
-                textAlign:"center",
-                padding:"15px"
-            }
-// external css from public folder style.css file
-
-
-//Functional component
-function Card(){
-    return <div className='card'>
-     <p className='cardTitle'>{todoTitle}</p>
-     <p className='cardDesc'>{todoDesc}</p>
-     <p className='cardDate'>{currentMonth+"/"+currentDate+"/"+currentYear}</p>
-     </div>
- }
-
 root.render(
-<div>
-    <Card/>
-    <Card/>
-    
-<p style={firstCss}>{"Year :"+currentYear}</p>
-<p className='DateCss'>{"Month :"+currentMonth}</p>
-<p>{"Date :"+currentDate}</p>
-<p>{"Time :"+currentTime}</p>
-</div>
+   <App/>
 );
 
