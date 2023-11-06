65686127327143c7ca51ea9c3a46c947e0ecd46b
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-11-06 22:40:49 +0600

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -8,13 +8,19 @@ import BindEvent from './Binding Event Handler/BindEvent';
 import Render from './Conditional_Rendering/Render';
 import State from './Conponents/State';
 import UseStateHooks from './Conponents/UseStateHooks';
+import DataFetch from './Custom Hooks/DataFetch';
+import DynamicStyle from './Dynamic Style/DynamicStyle';
 import EventHandler from './Event_Handler/EventHandler';
 import EventBubbling from './Event_Handler/Event_Bubbling';
+import FAQS from './FAQS (Freequently Asked Questions)/FAQS';
 import FormValidation from './Form Validation/FormValidation';
 import Form from './Form/Form';
 import Home from './State_Lifting/Home';
 import Parant from './State_Lifting/Parent';
+import Toastify from './Toastify/Toastify';
 import Toggle from './Toggle/Toggle';
+import DataFetc from './Use Effect/DataFetch';
+import UseEffect from './Use Effect/UseEffect';
 
 
 function App(){
@@ -35,7 +41,12 @@ function App(){
    <Home/>
    <FormValidation/>
    <Toggle/>
-   
+   <FAQS/>
+   <UseEffect/>
+   <DataFetc/>
+   <DataFetch/>
+   <Toastify/>
+   <DynamicStyle/>
 
 
     </div>
