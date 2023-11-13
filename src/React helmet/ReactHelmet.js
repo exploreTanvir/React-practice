265b520b4f745a980976c79ae2a7ab97ebd3582d
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-11-13 22:49:55 +0600

    React Helmet

---
+++ src/React helmet/ReactHelmet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Helmet} from "react-helmet"
+
+const ReactHelmet = () => {
+  return (
+    <div>
+        <Helmet><title>Hello</title></Helmet>
+    </div>
+  )
+}
+
+export default ReactHelmet
